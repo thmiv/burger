8d49acba8abf863c353d01cef98ce8fb1e370c51
thmiv <thmiv4@gmail.com>
2018-09-05 18:08:41 -0500

    repo initialization

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- values to be inserted into TABLE burgers --
+
+INSERT INTO burgers VALUES ('Hamburger');
+INSERT INTO burgers VALUES ('Cheeseburger');
+INSERT INTO burgers VALUES ('Chicken burger');
