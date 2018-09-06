005137ddd1bbccc1616f7f579a62864aeb025ddf
Thomas <thmiv4@gmail.com>
2018-09-06 14:46:36 -0500

    built some server functions

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,27 @@
+// connections information
+require("dotenv").config();
+const dbLogin = require("../login.js");
+//console.log(dbLogin);
 
+// Set up MySQL connection.
+const mysql = require("mysql");
 
-/*
-Host	gtizpe105piw2gfq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	
-Username	tmx0qrxhiawp83xc	
-Password	n2svco7dwcdczzqe	
-Port	3306	
-Database	vrogcgqqxfyly3sk
-*/
+const connection = mysql.createConnection({
+  host: dbLogin.eatDb.host,
+  port: 3306,
+  user: dbLogin.eatDb.username,
+  password: dbLogin.eatDb.password,
+  database: "vrogcgqqxfyly3sk"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
